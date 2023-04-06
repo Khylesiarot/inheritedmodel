ad958d010dabc26d868e15ae85c9fea6de27dcc8
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2023-04-06 15:44:50 +0800

    implemnt updateShouldNotify updateShouldNotifyDependent

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'dart:math' show Random;
+import 'dart:developer' as devtools show log;
 
 void main() {
   runApp(MaterialApp(
@@ -30,7 +31,44 @@ class _HomePageState extends State<HomePage> {
 
 enum AvailableColors { one, two }
 
+class AvailableColorsWidget extends InheritedModel<AvailableColors> {
+  final AvailableColors color1;
+  final AvailableColors color2;
 
+  AvailableColorsWidget(
+      {Key? key,
+      required this.color1,
+      required this.color2,
+      required Widget child})
+      : super(key: key, child: child);
+
+  static AvailableColorsWidget? of(
+      BuildContext context, AvailableColors aspect) {
+    return InheritedModel.inheritFrom(
+      context,
+      aspect: aspect,
+    );
+  }
+  
+  @override
+  bool updateShouldNotify(covariant AvailableColorsWidget oldWidget) {
+  
+    devtools.log('updateShouldNotify');  
+    return color1 != oldWidget.color1 || color2 != oldWidget.color2;
+  }
+  
+  @override
+  bool updateShouldNotifyDependent(covariant AvailableColorsWidget oldWidget, Set<AvailableColors> dependencies) {
+    devtools.log('updateShouldNotifyDependent');
+    if (dependencies.contains(AvailableColors.one) && color1 != oldWidget.color1){
+      return true;
+    }
+     if (dependencies.contains(AvailableColors.two) && color2 != oldWidget.color2){
+      return true;
+    }
+    return false;
+  }
+}
 
 final colors = [
   Colors.blue,
